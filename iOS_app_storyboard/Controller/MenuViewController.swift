6ba8ab7a740229a769a6261e79f3b53803029617
Van <van.viola25@gmail.com>
2020-03-19 14:05:11 +0800

    Re-upload

---
+++ iOS_app_storyboard/Controller/MenuViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MenuViewController.swift
+//  iOS_app_storyboard
+//
+//  Created by Viola, Christian Van Bagaporo on 17/03/2020.
+//  Copyright © 2020 deloitte. All rights reserved.
+//
+
+import UIKit
+
+
+enum MenuType: Int{
+    case home
+    case random
+    case gestures
+    case moved
+}
+class MenuViewController: UITableViewController {
+
+    var didTapMenuType : ((MenuType) -> Void)?
+       
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard  let menuType = MenuType(rawValue: indexPath.row) else { return }
+        dismiss(animated: true){ [weak self] in
+            print("Dismissing: \(menuType)")
+            
+            self?.didTapMenuType?(menuType)
+        }
+    }
+
+}
